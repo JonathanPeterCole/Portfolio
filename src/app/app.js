0a94d93563048748913a84d6dff58c10f822f3d5
Jonathan Cole <JonathanPeterCole@gmail.com>
2019-05-26 16:13:52 +0100

    Implemented theme localStorage

--- src/app/app.js
+++ src/app/app.js
@@ -5,13 +5,21 @@ export default class App extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
-      darkTheme: false
+      darkTheme: JSON.parse(localStorage.getItem('darkTheme'))
     }
   }
+  
+  toggleDarkTheme = () => {
+    localStorage.setItem('darkTheme', !this.state.darkTheme)
+    this.setState({
+      darkTheme: !this.state.darkTheme
+    })
+  }
+
   render () {
     return (
       <div className={classNames('theme-provider', {dark: this.state.darkTheme})}>
-        <div className='app'>
+        <div className='app' onClick={this.toggleDarkTheme}>
           Test App
         </div>
       </div>
