92513dd8a44adabeb3623357d3bef9d059e0da54
Jonathan Cole <JonathanPeterCole@gmail.com>
2019-05-26 15:51:08 +0100

    Added theme toggle state

--- src/app/app.js
+++ src/app/app.js
@@ -1,9 +1,20 @@
 import React from 'react'
+import classNames from 'classnames'
 
 export default class App extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      darkTheme: false
+    }
+  }
   render () {
     return (
-      <div>Test App</div>
+      <div className={classNames('theme-provider', {dark: this.state.darkTheme})}>
+        <div className='app'>
+          Test App
+        </div>
+      </div>
     )
   }
 }
