b5e380e6db2cdc7f9270eded331a6951257a38d6
Jonathan Cole <JonathanPeterCole@gmail.com>
2019-06-01 18:33:21 +0100

    Added project hover effect

--- src/app/components/projects/projects.js
+++ src/app/components/projects/projects.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import classNames from 'classnames'
-import ProjectTile from './project-tile';
+import ProjectTile from './project-tile'
 
 const Projects = props => {
   return (
