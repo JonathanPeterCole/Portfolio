3315af066ba9cf4a60192c059675757e9c5ee285
Jonathan Cole <JonathanPeterCole@gmail.com>
2019-06-09 19:32:30 +0100

    Added prototype screenshots

--- src/app/pages/clean-air/clean-air.js
+++ src/app/pages/clean-air/clean-air.js
@@ -6,6 +6,10 @@ import Tags from '../../components/tags/tags'
 
 import GalleryImg1 from './gallery/clean-air-1.jpg'
 import GalleryImg2 from './gallery/clean-air-2.jpg'
+import GalleryImg3 from './gallery/clean-air-3.jpg'
+import GalleryImg4 from './gallery/clean-air-4.jpg'
+import GalleryImg5 from './gallery/clean-air-5.jpg'
+import GalleryImg6 from './gallery/clean-air-6.jpg'
 
 export default class CleanAirPage extends React.Component {
   render () {
@@ -24,6 +28,26 @@ export default class CleanAirPage extends React.Component {
             position: 1,
             image: GalleryImg2,
             caption: 'Final Product - Profile Page'
+          },
+          {
+            position: 2,
+            image: GalleryImg3,
+            caption: 'Personal Prototype - Explore Page'
+          },
+          {
+            position: 3,
+            image: GalleryImg4,
+            caption: 'Personal Prototype - Profile Page'
+          },
+          {
+            position: 4,
+            image: GalleryImg5,
+            caption: 'Personal Prototype - History Page'
+          },
+          {
+            position: 5,
+            image: GalleryImg6,
+            caption: 'Personal Prototype - Settings Page'
           }
         ]}/>
         <Tags tags={['Express', 'ReactJS', 'SASS', 'Android', 'AWS', 'Terraform']} />
