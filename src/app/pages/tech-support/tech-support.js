7062f6859eda608430b16943fe68141e7a6347e7
Jonathan Cole <JonathanPeterCole@gmail.com>
2019-06-09 17:07:41 +0100

    Added tech support page

--- src/app/pages/tech-support/tech-support.js
+++ src/app/pages/tech-support/tech-support.js
@@ -1,6 +1,14 @@
 import React from 'react'
 
 import DetailsPage from '../../components/details/details-page'
+import Gallery from '../../components/gallery/gallery'
+import Tags from '../../components/tags/tags'
+import ActionLinks from '../../components/action-links/action-links'
+
+import GalleryImg1 from './gallery/tech-support-site-1.jpg'
+import GalleryImg2 from './gallery/tech-support-site-2.jpg'
+import GalleryImg3 from './gallery/tech-support-site-3.jpg'
+import GalleryImg4 from './gallery/tech-support-site-4.jpg'
 
 export default class TechSupportPage extends React.Component {
   render () {
@@ -9,7 +17,35 @@ export default class TechSupportPage extends React.Component {
         title='Tech Support Site'
         subheading='A website to advertise tech support services and provide easy email bookings.'
         gradient={['#db2525', '#9e0538']}>
-        Content will go here...
+        <Gallery accentColor='#db2525' images={[
+          {
+            position: 0,
+            image: GalleryImg1,
+            caption: 'Homepage'
+          },
+          {
+            position: 1,
+            image: GalleryImg2,
+            caption: 'Booking Page - Service Selection'
+          },
+          {
+            position: 2,
+            image: GalleryImg3,
+            caption: 'Booking Page - On/Off Site Selection'
+          },
+          {
+            position: 3,
+            image: GalleryImg4,
+            caption: 'Booking Page - Booking Form'
+          }
+        ]}/>
+        <Tags tags={['Flask', 'JQuery', 'CSS', 'Digital Ocean']} />
+        <ActionLinks links={[
+          {
+            name: 'Source Code',
+            url: 'https://github.com/JonathanPeterCole/Tech-Support-Site'
+          }
+        ]} />
       </DetailsPage>
     )
   }
