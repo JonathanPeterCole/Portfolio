b059fde4a646be15080c91640ca10025b70bc8d1
Jonathan Cole <JonathanPeterCole@gmail.com>
2019-06-10 14:20:01 +0100

    Updated action links to open in a new tab

--- src/app/components/action-links/action-links.js
+++ src/app/components/action-links/action-links.js
@@ -6,7 +6,7 @@ const ActionLinks = props => {
     <div className='action-links'>
       <div className='action-link-container'>
         {props.links.map((link, key) => (
-          <a key={key} className='action-link' href={link.url}>{link.name}</a>
+          <a key={key} className='action-link' href={link.url} target='_blank'>{link.name}</a>
         ))}
       </div>
     </div>
